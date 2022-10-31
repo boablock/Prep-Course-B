9a4324403c1c4cd8eb298af5c0dbd3d35251b4e9
boablock <boablockchain@gmail.com>
2022-10-31 16:47:21 -0300

    homework IV done

--- practice/JS-IIII.js
+++ practice/JS-IIII.js
@@ -59,7 +59,61 @@ console.log(Object.values(person));
 
 for (let key in person){
     console.log(key); // key 
-    console.log(person[key]); // value key
-    // console.log(person.key); // dot notation vs braket notation: when using dot notation should be named the value of the property, variable cannot be named to value accessing when using dot notation. In that case, we should use bracket notation: console.log(person[key]) or console.log(person[age]);
+    console.log(person[key]); // dot notation: we should use dot notation only with the exacly property name. For access to a property throght a variable name (like key), we should use bracket notation. We use dot notation when we know object properties. 
+    // console.log(person.key); // dot notation vs braket notation: when using dot notation should be named the exacly property name, variable cannot be named for property accessing when using dot notation. In that case, we should use bracket notation: console.log(person[key]).
     // console.log(person.religion); 
+}
+
+
+//1)
+    // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
+    // De lo contratio, devuelve "false"
+    // Tu código:
+function tieneEmail (usuario) {
+      if(usuario.email === null || usuario.email === undefined){
+        return false;
+      }else{
+        return true;
+      }
+    }
+
+function tieneEmail (usuario) {
+  for (key in usuario){ 
+    if(usuario.email){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
+//2)--> creat the object 'usuario' and use it like a parameter calling the function sumarLikesDeUsuario
+  // "usuario" tiene una propiedad llamada "posts" que es un array
+  // "posts" es un array de objetos "post"
+  // Cada objeto "post" tiene una propiedad llamada "likes" que es un entero (int/integer)
+  // Suma todos los likes de todos los objetos "post"
+  // Devuelve la suma
+  // Tu código:
+function sumarLikesDeUsuario (usuario) {
+  suma =0;
+  for(let i = 0; i < usuario.posts.length; i++){
+    suma = suma + usuario.posts[i].likes;
+  }
+  return suma; 
+}
+//3) 
+  // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
+  // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
+  // El método resta el descuento del precio y devuelve el precio con descuento
+  // Devuelve el objeto "producto" al final de la función
+  // Ejemplo:
+  // producto.precio -> 20
+  // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
+  // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
+  // Tu código:
+function agregarMetodoCalculoDescuento (producto) {
+  producto.calcularPrecioDescuento = function (){
+    return producto.precio - (producto.precio * producto.porcentajeDeDescuento);
+    
+  }
+  return producto; 
 }
