74117a356f9dbca5e0701d0a9e3693ded50dd9d6
boablock <boablockchain@gmail.com>
2022-10-31 09:17:54 -0300

    second commit

---
+++ practice/JS-IIII.js
@@ -0,0 +1,11 @@
+person ={
+    name: 'Mark',
+    age: 22,
+    email: 'jpboada@gmail.com',
+    religion: 'Atheist',
+    nationality: 'Swiss',
+    hobbies: ['Tennis', 'Study', 'Dance', 'sex', 'meditation'],
+    married : false
+}
+
+console.log(person[name]);
